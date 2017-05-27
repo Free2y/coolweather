3efe6dd2431de70fbe1139b2d5faef2cbd7c128d
Freeruzy <262578284@qq.com>
2017-05-27 16:39:33 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/freeruzy/android/coolweather/db/County.java
@@ -0,0 +1,47 @@
+package com.freeruzy.android.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Freeruzy on 2017/5/27.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
